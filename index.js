f627b3e6c6dba37cc88e755e6d79f2902006ef5c
Morse Iván Méndez González <1962689580101@ingenieria.usac.edu.gt>
2026-07-31 17:23:34 -0600

    Tarea1_200011036

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        nombre: "Morse Iván Méndez González ----------------------", 
+        cancionFavorita: "La retirada"
+    });
+});
+
+const PORT = 18000;
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
+
